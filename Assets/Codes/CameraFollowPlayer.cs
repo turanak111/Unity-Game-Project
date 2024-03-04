d52167491d91fb2497924e383cbb06d0efe9e75f
SefaSelim <sefaselimtura@gmail.com>
2024-03-04 13:54:09 +0300

    Added Camera Following Mechanic (Code Script in Camera Object Not In Player) and added some shapes for testing (Will Be Remove)

---
+++ Assets/Codes/CameraFollowPlayer.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollowPlayer : MonoBehaviour
+{
+    GameObject player;
+    bool followPlayer = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (followPlayer == true)
+        {
+            camFollowPlayer();
+        }
+    }
+
+    public void setFollowPlayer(bool val)
+    {
+        followPlayer = val;
+    }
+
+    void camFollowPlayer()
+    {
+            Vector3 newPos = new Vector3(player.transform.position.x,player.transform.position.y,this.transform.position.z);
+            this.transform.position = newPos;
+    }
+
+}
